8a37ff870bb5c760c965f5f32ef3f3b7479370f7
liugaxin1118 <liugaxin1118@gmail.com>
2020-05-17 00:20:25 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose') // 載入 mongoose
 
 const app = express()
 
+const exphbs = require('express-handlebars');
+
+
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
@@ -18,9 +21,12 @@ db.on('error', () => {
     console.log('mongodb connected!')
   })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000

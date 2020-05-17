a07f81b294373e76cd33dbee277b016fb4368077
liugaxin1118 <liugaxin1118@gmail.com>
2020-05-17 11:34:55 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -42,6 +42,14 @@ app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('detail', { todo }))
+      .catch(error => console.log(error))
+  })
+
 app.post('/todos', (req, res) => {
     const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
     return Todo.create({ name })     // 存入資料庫

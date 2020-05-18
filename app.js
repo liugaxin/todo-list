9fdf5a9f66f194bd4ff96973d40a51525911917a
liugaxin1118 <liugaxin1118@gmail.com>
2020-05-18 12:54:27 +0800

    feat: model todo add isDone params

--- app.js
+++ app.js
@@ -68,13 +68,14 @@ app.get('/todos/:id/edit', (req, res) => {
 
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(()=> res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 

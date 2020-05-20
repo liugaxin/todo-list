ced670772f831ac61670e1bc883c574da3c68f68
liugaxin1118 <liugaxin1118@gmail.com>
2020-05-20 15:33:36 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 // 載入 method-override
-const methodOverride = require('method-override') 
+const methodOverride = require('method-override')
+
+
 
 // 引用路由器
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
+const PORT = process.env.PORT || 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))  
@@ -19,6 +25,7 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 // 設定 port 3000
-app.listen(3000, () => {
+// 設定應用程式監聽的埠號
+app.listen(PORT, () => {
   console.log('App is running on http://localhost:3000')
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// config/mongoose.js
+// 如果在 Heroku 環境則使用 process.env.MONGODB_URI
+// 否則為本地環境，使用 mongodb://localhost/todo-list
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')

c43fb5ff04c5ca9b638ba0495c7439b022e2503b
liugaxin1118 <liugaxin1118@gmail.com>
2020-05-20 12:00:02 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
-
-// 引用路由器
-const routes = require('./routes')
-
-const app = express()
-
 const exphbs = require('express-handlebars');
-
 const bodyParser = require('body-parser')
-
-const Todo = require('./models/todo') // 載入 Todo model
-
 // 載入 method-override
 const methodOverride = require('method-override') 
 
+// 引用路由器
+const routes = require('./routes')
+require('./config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-  })
-  // 連線成功
-  db.once('open', () => {
-    console.log('mongodb connected!')
-  })
-
+const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))  
-
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
 // 將 request 導入路由器
 app.use(routes)
-
 
 // 設定 port 3000
 app.listen(3000, () => {
